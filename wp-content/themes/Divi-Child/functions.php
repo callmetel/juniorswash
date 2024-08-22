eee09cb2911578c23d71858d37d1807ce7cd5e07
Tel <tel@jsdfyodds.com>
2024-08-22 14:29:55 -0400

    fixed desktop header css & booking nav

--- wp-content/themes/Divi-Child/functions.php
+++ wp-content/themes/Divi-Child/functions.php
@@ -21,6 +21,9 @@
 function divichild_enqueue_scripts()
 {
 	wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+	wp_enqueue_style('child-style', get_stylesheet_directory_uri() . '/css/style.css');
+	wp_enqueue_style('bootstrap-css', 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css', array(), false, '');
+	wp_enqueue_script('bootstrap-js', 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js', array(), false, '');
 }
 add_action('wp_enqueue_scripts', 'divichild_enqueue_scripts');
 
